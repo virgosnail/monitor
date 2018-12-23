4e03c497d0e61033438d3697ac8cb5569f67a317
virgosnail <919333549@qq.com>
2018-12-23 15:12:19 +0800

    服务端 Config.java

---
+++ server/src/main/java/com/skd/server/config/Config.java
@@ -0,0 +1,15 @@
+package com.skd.server.config;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description
+ * @Author virgosnail
+ * @Date 2018/12/22 10:36
+ */
+@Component
+@Data
+public class Config {
+    private String rootPath;
+}
